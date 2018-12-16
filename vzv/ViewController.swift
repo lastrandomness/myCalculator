764bd606d5ee23d5542037977e430e3af3f245b3
TikTak <llim37121@gmail.com>
2018-12-16 10:31:02 -0800

    new one

--- vzv/ViewController.swift
+++ vzv/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textName: UITextField!
+    @IBOutlet weak var labelShowName: UILabel!
+    @IBAction func buttonClick(_ sender: Any) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
 }
 
+
+
+
+/*let msg = UIAlertController(title: "hello", message: "what's your name", preferredStyle:.alert)
+ let msg2 = UIAlertAction(title: "hallo", style: .default){ (action) in print("This is msg 2!")}
+ present(msg,animated: true,completion: nil)
+ msg.addAction(msg2)*/
